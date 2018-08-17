cbc737450cdb6d2147cef4619dac29c910ee2266
Larry <Nightmachine@gmail.com>
2018-08-17 11:37:47 -0600

    input boxes on wizard

--- src/component/wizard/Wizard.js
+++ src/component/wizard/Wizard.js
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Wizard extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            name: '',
+            address: '',
+            city: '',
+            state: '',
+            zipcode: '',
+        }
+    }
+
+    updateName(val) {
+        this.setState({ name: val })
+
+    }
+
+    updateAddress(val) {
+        this.setState({ address: val })
+
+    }
+
+    updateCity(val) {
+        this.setState({ city: val })
+
+    }
+
+    updateState(val) {
+        this.setState({ state: val })
+
+    }
+
+    updateZipCode(val) {
+        this.setState({ zipcode: val })
+
+    }
+
     render() {
         return (
             <div>
-                <h1>Wizard</h1>
+
+                <input onChange={(e) => this.updateName(e.target.value)} />
+                <input onChange={(e) => this.updateAddress(e.target.value)} />
+                <input onChange={(e) => this.updateCity(e.target.value)} />
+                <input onChange={(e) => this.updateState(e.target.value)} />
+                <input onChange={(e) => this.updateZipCode(e.target.value)} />
+
+
                 <Link to='/'><button>Cancel</button></Link>
+
+
             </div>
         )
     }
